5cd0752cf131fd6973f769cc9c7b3f57303b79a6
manonmission88 <manishnewray88@gmail.com>
2025-03-16 16:39:42 -0400

    Added function to find block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,15 +1,48 @@
-def markdown_to_blocks(markdown):
+"""
+It takes a raw Markdown string (representing a full document) 
+as input and returns a list of "block" strings.
 
+"""
+from enum import Enum 
+class BlockType(Enum):
+    PARAGRAPH = 'paragraph'
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    ORDERED_LIST = 'ordered_list'
+    UNORDERED_LIST = 'unordered_list'
+    
+    
+    
+def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
-    filtered_markdown = []
+    final_markdown = []
     for block in blocks:
         if len(block) == 0:
             continue
-        block = block.strip()
-        filtered_markdown.append(block)
-    return filtered_markdown
+        final_markdown.append(block.strip())
+    return final_markdown
 
-
+def block_to_block_type(block):
+    print(block)
+    if block.startswith('#'):
+        return BlockType.HEADING
+    if block.startswith('>'):
+        return BlockType.QUOTE
+    if block.startswith('```') and block.endswith('```'):
+        return BlockType.CODE
+    if block.startswith('-'):
+        return BlockType.UNORDERED_LIST
+    ordered_markers = ('1', 'A', 'a', 'I', 'i')
+    if block.startswith(ordered_markers):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+    
+    
+    
+    
+    
+    
 text = """# This is a heading
 
 This is a paragraph of text. 
@@ -18,4 +51,6 @@ It has some **bold** and _italic_ words inside of it.
 - This is the first list item in a list block
 - This is a list item
 - This is another list item"""
-print(markdown_to_blocks(text))
+# markdown_blocks = markdown_to_blocks(text)
+# for block in markdown_blocks:
+#     print(block,block_to_block_type(block))
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,4 +1,4 @@
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks,block_to_block_type,BlockType
 import unittest
 
 
@@ -45,5 +45,26 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+        
+    def test_block_to_block_type(self):
+        md = "- This is a list\n- with items"
+        self.assertEqual(block_to_block_type(md),BlockType.UNORDERED_LIST)
+    
+    def test_block_to_block_types(self):
+        block = "######## heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "A. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+        
 if __name__ == "__main__":
     unittest.main()
